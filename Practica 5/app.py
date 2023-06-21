30a263144fafff8f642490f2e69e14ddefea90e8
LuisG-Custodio <121039302@upq.edu.mx>
2023-06-21 17:07:09 -0600

    Almacenamiento y mensaje
    Se generó el codigo para el almacenamiento dentro de la base de datos de mysql, y que se pudiera mostrar un mensaje al realizarse, a la vez que regresa al index

--- Practica 5/app.py
+++ Practica 5/app.py
@@ -1,5 +1,5 @@
 #importación del framework
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 #inicialización del APP
@@ -9,6 +9,7 @@ app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
 app.config['MYSQL_DB']='dbflask'
+app.secret_key= 'mysecretkey'
 mysql= MySQL(app)
 
 #declaración de ruta / http://localhost:5000
@@ -20,12 +21,17 @@ def index():
 @app.route('/guardar', methods=['POST'])
 def guardar():
     if request.method == 'POST':
-        titulo=request.form['txtTitulo']
-        artista=request.form['txtArtista']
-        anio=request.form['txtAnio']
-        print(titulo, artista, anio)
-    
-    return "Los datos llegaron Amigo ;)"
+        #pasamos a variables el contendio de los input
+        Vtitulo=request.form['txtTitulo']
+        Vartista=request.form['txtArtista']
+        Vanio=request.form['txtAnio']
+        
+        #Conectar y ejecutar el insert
+        CS= mysql.connection.cursor()
+        CS.execute('insert into tb_albums(titulo,artista,anio) values(%s,%s,%s)',(Vtitulo,Vartista,Vanio))
+        mysql.connection.commit()           
+    flash('Los datos del album fueron agregados correctamente')
+    return redirect(url_for('index'))
 
 @app.route('/eliminar')
 def eliminar():
